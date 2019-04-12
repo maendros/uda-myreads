b86c6feb230c50b149d3d91b48d9a97a2524ecb3
maendros <valitsianius@hotmail.com>
2019-04-12 18:39:37 +0300

    pushing project to git

---
+++ build/precache-manifest.88696afc2550839abac3e9e39689af65.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/uda-myreads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/uda-myreads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/uda-myreads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "7776d46312b2b6f76517",
+    "url": "/uda-myreads/static/js/runtime~main.c1350bee.js"
+  },
+  {
+    "revision": "204bcba02d6103ff1e5c",
+    "url": "/uda-myreads/static/js/main.af25d2be.chunk.js"
+  },
+  {
+    "revision": "711a09716828c1ec9a30",
+    "url": "/uda-myreads/static/js/2.386a1cf6.chunk.js"
+  },
+  {
+    "revision": "204bcba02d6103ff1e5c",
+    "url": "/uda-myreads/static/css/main.b77adfc2.chunk.css"
+  },
+  {
+    "revision": "ef0fdf2d1d8b1302e475bdf9176950d7",
+    "url": "/uda-myreads/index.html"
+  }
+];
